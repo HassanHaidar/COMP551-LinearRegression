ab2c823edb0883e988ef37f197b7de9108b62798
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2019-01-14 14:51:58 -0500

    allow user input

--- LinearRegression/LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression/LinearRegression.py
@@ -1,33 +1,39 @@
 import numpy as np
 
-#define an example set of X, of size 10 x 2
-# every x has 1 fearure, but we add 1 feature of value 1 as a bias (intercept)
-# without the bias, size of x would be 10 x 1
-X = np.array([[0.86,0.09,-0.85,0.87,-0.44,-0.43,-1.1,0.40,-0.96,0.17],[1.0 for i in range(10)]])
+# obtain input data x from user
+# every x has 1 feature, but we add 1 feature of value 1 as a bias (intercept)
+x_data =  input("Enter the x data as x1 x2 ... xn: ")
+x_data = x_data.split();
+x_data = list(map(float, x_data))
 
-print(X.shape)
+X = np.array([x_data,[1.0 for i in range(len(x_data))]])
+X = np.transpose(X);
 
-#define an example set of target y, of size 10 x 1
-Y = np.array([2.49,0.83,-0.25,3.10,0.87,0.02,-0.12, 1.81, -0.83, 0.43])
-Y = Y.reshape(10,1)
 
-#define initial function f of size 1 x 2
-W = np.array([1, 1], dtype = float)
+# obtain data y
+y_data = input("Enter the corresponding y data. Make sure you enter the same number of data for y as for x: ") #request input from user
+y_data = y_data.split()
+Y = list(map(float, y_data))
+if (len(Y) != len(X)):
+    print("Different number of data!")
+else:
+    Y = np.array(y_data, float)
 
-# find the value of (transpose of X)*(X)
-xt_x = X @ np.transpose(X)
-print(xt_x.shape)
+    Y = Y.reshape(Y.size,1)
 
-# find the value of the previous value
-xt_x_inverse = np.linalg.inv(xt_x)
-print(xt_x_inverse.shape)
+    # find the value of (transpose of X)*(X)
+    xt_x = np.transpose(X) @ X
 
-# weights = (x^T * x)^(-1) * x^T * Y
-W = (xt_x_inverse @ X) @ Y
+    # find the value of the previous value
+    xt_x_inverse = np.linalg.inv(xt_x)
 
-print(W.shape)
+    # weights = (x^T * x)^(-1) * x^T * Y
+    W = (xt_x_inverse @ np.transpose(X)) @ Y
 
-print("Best fit line for the input data is y = {}x + {}".format(W[0][0],W[1][0]))
+    print("Best fit line for the input data is y = {}x + {}".format(W[0][0],W[1][0]))
+
+ 
+
 
 
 
